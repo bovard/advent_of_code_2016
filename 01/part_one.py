d4fc2eb09c1d35383a6e25428f4074ea559bfa7f
Bovard Tiberi <bovard.tiberi@gmail.com>
2016-12-01 18:41:51 -0800

    added comments

--- 01/part_one.py
+++ 01/part_one.py
@@ -4,17 +4,23 @@ from maps import Direction, Location
 with open('input.txt') as f:
     instructions = f.read().strip().split(', ')
 
+# start at 0,0 facing North
 start = Location(0, 0)
-current = start
 direction = Direction.NORTH
 
+current = start
 for inst in instructions:
     lr = inst[0]
     n = int(inst[1:])
+
+    # turn left or right
     if lr == 'L':
         direction = Direction.rotate_left(direction)
     elif lr == 'R':
         direction = Direction.rotate_right(direction)
+
+    # walk n units forward
     current = current.add(direction, n)
 
+# report the distance back to the beginning
 print start.distance_to_loc(current)
--- 01/part_two.py
+++ 01/part_two.py
@@ -4,20 +4,24 @@ from maps import Direction, Location
 with open('input.txt') as f:
     instructions = f.read().strip().split(', ')
 
+# start at 0,0 facing north
 start = Location(0, 0)
-current = start
 direction = Direction.NORTH
 
+# keep track of our current location, and all locations visited
+current = start
 visited = [start]
-i = 0
 for inst in instructions:
-    i += 1
     lr = inst[0]
     n = int(inst[1:])
+
+    # turn left
     if lr == 'L':
         direction = Direction.rotate_left(direction)
     elif lr == 'R':
         direction = Direction.rotate_right(direction)
+
+    # one square at a time walk forward, checking if we cross our old path
     for i in range(n):
         current = current.add(direction, 1)
         if current in visited:
